852484f0b53d03173ef00706f7aac637812ca2b0
MeerimB <meerimb312@gmail.com>
2024-05-30 22:23:09 +0300

    update gitignore,pom.xml,Test1.java

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,9 +1,17 @@
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Test1 {
 
     @Test
-    public void testMethod(){
+    public void testPass() {
 
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void testFail() {
+
+        Assert.assertTrue(false);
     }
 }
