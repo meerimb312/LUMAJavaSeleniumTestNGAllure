d50d70a459d61df7cd3ea46dc3462cf73f101ddd
MeerimB <162055160+meerimb312@users.noreply.github.com>
2024-06-09 14:58:58 +0300

    Add Cross Browser Testing (#13)
    * Add Cross Browser Testing

--- src/test/java/com/lumatest/utils/DriverUtils.java
+++ src/test/java/com/lumatest/utils/DriverUtils.java
@@ -3,12 +3,21 @@ package com.lumatest.utils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chromium.ChromiumOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import java.util.Map;
 
 public class DriverUtils {
 
-    private static final ChromeOptions chromeOptions;
+    private static ChromeOptions chromeOptions;
+    private static FirefoxOptions firefoxOptions;
+    private static ChromiumOptions<ChromeOptions> chromiumOptions;
+    private static EdgeOptions edgeOptions;
+
 
     static {
         chromeOptions = new ChromeOptions();
@@ -22,27 +31,95 @@ public class DriverUtils {
         chromeOptions.addArguments("--disable-web-security");
         chromeOptions.addArguments("--allow-running-insecure-content");
         chromeOptions.addArguments("--ignore-certificate-errors");
+
+        firefoxOptions = new FirefoxOptions();
+
+        firefoxOptions.addArguments("--incognito");
+        firefoxOptions.addArguments("--headless");
+        firefoxOptions.addArguments("--window-size=1920,1080");
+        firefoxOptions.addArguments("--disable-gpu");
+        firefoxOptions.addArguments("--no-sandbox");
+        firefoxOptions.addArguments("--disable-dev-shm-usage");
+        firefoxOptions.addArguments("--disable-web-security");
+        firefoxOptions.addArguments("--allow-running-insecure-content");
+        firefoxOptions.addArguments("--ignore-certificate-errors");
+
+        chromiumOptions = chromeOptions;
+
+        edgeOptions = new EdgeOptions();
+
+        edgeOptions.addArguments("--incognito");
+        edgeOptions.addArguments("--headless");
+        edgeOptions.addArguments("--window-size=1920,1080");
+        edgeOptions.addArguments("--disable-gpu");
+        edgeOptions.addArguments("--no-sandbox");
+        edgeOptions.addArguments("--disable-dev-shm-usage");
+        edgeOptions.addArguments("--disable-web-security");
+        edgeOptions.addArguments("--allow-running-insecure-content");
+        edgeOptions.addArguments("--ignore-certificate-errors");
+
     }
 
-    //for Luma
-    public static WebDriver createChromeDriver(WebDriver driver) {
+    private static WebDriver createChromeDriver(WebDriver driver) {
         if (driver != null) {
             driver.quit();
+
+            return driver;
         }
         ChromeDriver chromeDriver = new ChromeDriver(chromeOptions);
         chromeDriver.executeCdpCommand("Network.enable", Map.of());
         chromeDriver.executeCdpCommand("Network.setExtraHTTPHeaders", Map.of("headers", Map.of("accept-language", "en-US,en;q=0.9")));
-        return new ChromeDriver(chromeOptions);
+
+        return chromeDriver;
+    }
+
+    private static WebDriver createEdgeDriver(WebDriver driver) {
+        if (driver != null) {
+            driver.quit();
+
+            return driver;
+        }
+        EdgeDriver edgeDriver = new EdgeDriver(edgeOptions);
+        edgeDriver.executeCdpCommand("Network.enable", Map.of());
+        edgeDriver.executeCdpCommand("Network.setExtraHTTPHeaders", Map.of("headers", Map.of("accept-language", "en-US,en;q=0.9")));
+
+        return edgeDriver;
+    }
+
+
+    private static WebDriver createFirefoxDriver(WebDriver driver) {
+        if (driver != null) {
+            driver.quit();
+
+            return driver;
+        }
+        FirefoxDriver firefoxDriver = new FirefoxDriver(firefoxOptions);
+//        firefoxDriver.executeScript("Network.enable", Map.of());
+//        firefoxDriver.executeScript("Network.setExtraHTTPHeaders", Map.of("headers", Map.of("accept-language", "en-US,en;q=0.9")));
+
+        return firefoxDriver;
+    }
+
+    public static WebDriver createDriver(String browser, WebDriver driver) {
+        switch (browser) {
+            case "chrome" -> {
+                return createChromeDriver(driver);
+            }
+            case "firefox" -> {
+                return createFirefoxDriver(driver);
+            }
+            case "chromium" -> {
+                return createChromeDriver(driver);
+            }
+            case "edge" -> {
+                return createEdgeDriver(driver);
+            }
+
+            default -> {
+                return null;
+            }
+        }
     }
 }
 
-//    for open cart
-//    public static WebDriver createChromeDriver(WebDriver driver) {
-//        if (driver == null) {
-//            return new ChromeDriver(chromeOptions);
-//        } else {
-//            driver.quit();
-//            return new ChromeDriver(chromeOptions);
-//        }
-//    }
 
