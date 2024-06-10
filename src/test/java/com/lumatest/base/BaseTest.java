cd6d809a69eccf35b9a7a7385a76480d1477f515
MeerimB <162055160+meerimb312@users.noreply.github.com>
2024-06-10 20:47:46 +0300

    add parallel test,Report utils (#14)
    * add parallel test,ReportUtils

--- src/test/java/com/lumatest/base/BaseTest.java
+++ src/test/java/com/lumatest/base/BaseTest.java
@@ -1,13 +1,13 @@
 package com.lumatest.base;
 
 import com.lumatest.utils.DriverUtils;
+import com.lumatest.utils.ReportUtils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
 import org.testng.Reporter;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Parameters;
+import org.testng.annotations.*;
+
 
 public abstract class BaseTest {
     private WebDriver driver;
@@ -16,16 +16,17 @@ public abstract class BaseTest {
     protected void setupWebDriverManager() {
         WebDriverManager.chromedriver().setup();
         WebDriverManager.chromiumdriver().setup();
-        WebDriverManager.edgedriver().setup();
+//        WebDriverManager.edgedriver().setup();
 //        WebDriverManager.firefoxdriver().setup();
 
     }
 
     @Parameters("browser")
     @BeforeMethod
-    protected void setupDriver(String browser) {
+    protected void setupDriver(@Optional("chrome") String browser, ITestResult result) {
         Reporter.log("---------------------------------------------------------", true);
         this.driver = DriverUtils.createDriver(browser, this.driver);
+        Reporter.log("RUN " + result.getMethod().getMethodName(), true);
 
         if (getDriver() == null) {
             Reporter.log("Error: Unknown parameter 'browser' - " + browser + ". ", true);
@@ -38,8 +39,12 @@ public abstract class BaseTest {
 
     @Parameters("browser")
     @AfterMethod(alwaysRun = true)
-    protected void tearDown(String browser) {
-        if (this.driver != null) {
+    protected void tearDown(@Optional("chrome") String browser, ITestResult result) {
+        Reporter.log(result.getMethod().getMethodName() + ": " + ReportUtils.getTestStatus(result),
+                true);
+
+        if (getDriver() != null) {
+
             getDriver().quit();
             Reporter.log("INFO: " + browser.toUpperCase() + " driver closed.", true);
 
